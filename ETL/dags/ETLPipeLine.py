ed766c949b0370d7d1f86a591ddd29bed252b6e4
Jeevan <jeevanaher732@gmail.com>
2025-01-24 15:52:31 -0600

    Deploying the ETL Project

--- ETL/dags/ETLPipeLine.py
+++ ETL/dags/ETLPipeLine.py
@@ -1,14 +1,17 @@
 from airflow import DAG
-from airflow.providers.http.operators.http import SimpleHTTPOperator
-from airflow.operators import task
+from airflow.providers.http.operators.http import SimpleHttpOperator
+
+
+from airflow.decorators import task
+
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.utils.dates import days_ago
 import json 
 
 with DAG(
-    dag_id="Nasa_apod_postgress",
+    dag_id="nasa_apod_postgres",
     start_date=days_ago(1),
-    schedule_interval="@daily",
+    schedule="@daily",
     catchup=False
 
 
@@ -18,9 +21,10 @@ with DAG(
     @task
     def create_table():
         #initialize the Postgress Hook: it is used to intract with the Postgress server
-        postgress_Hook = PostgresHook(postgress_conn_id="my_postgress_connetion")
+        postgress_Hook = PostgresHook(postgres_conn_id='my_postgres_connection')
 
-        create_table_query="""CREATE TABLE IF NOT EXIST apod_data (
+        create_table_query="""CREATE TABLE IF NOT EXISTS apod_data (
+ 
         id SERIAL PRIMARY KEY,
         title  VARCHAR(255),
         explanation TEXT,
@@ -32,23 +36,65 @@ with DAG(
 
         """
 
-        postgres_hook.run(create_table_query)
+        postgress_Hook.run(create_table_query)
 
     #step 2: Extract the NASA api Data(APOD)-Astronomy picrure of the day
+    #Api_key=https://api.nasa.gov/planetary/apod?api_key=HwhXybGdOhtHGVwmm0GLQ8biJaQ4Zhjdj9TVMsf7
  
-    extract_apod=SimpleHTTPOperator(
+    extract_apod=SimpleHttpOperator(
         task_id='task_id',
-        http_conn_id='nasa_api',
-        endpoint='planetary/apod'
+        http_conn_id='nasa_api',# connection id defined in Airflow for NASA API
+        endpoint='planetary/apod', ## NASA API endpoint for APOD(Astronomy Picture Of the Day)
+        method="GET",
+        data={"api_key": "{{ conn.nasa_api.extra_dejson['api_key'] }}"}, # using the API key from the connection 
+        response_filter=lambda response:response.json(), #convert response to the Json using Lambda
     )
     #Step 3: Transform the Data ,Pick the information that i need to save
+    @task
+    def transform_apod_data(response):
+        apod_data = {
+            'title': response.get('title', ''),
+            'explanation': response.get('explanation', ''),  # fixed typo
+            'url': response.get('url', ''),
+            'date': response.get('date', ''),  # fixed the key
+            'media_type': response.get('media_type', ''),
+        }
+        return apod_data
+
     
+
+
+
     #Step:4  : Load the Data Into PostgressSql
 
+    @task
+    def load_data_to_postgres(apod_data):
+        #initialize the PostgresHook
+        postgres_hook=PostgresHook(postgres_conn_id='my_postgres_connection')
 
-    #step5: Verify the data DBViewer
+        #define SQL insert Query
+        insert_query="""INSERT INTO apod_data (title,explanation, url ,date ,media_type) VALUES (%s,%s,%s,%s,%s);
+                """
+
+        #execute the SQL query
+        postgres_hook.run(insert_query,parameters=(
+            apod_data['title'],
+            apod_data['explanation'],
+            apod_data['url'],
+            apod_data['date'],
+            apod_data['media_type'],
+
+        ))
+    #step5: Verify the data DBViewer   
+     
 
 
 
     #step 6: Define the Task Dependancies
+    #Extract
+
+    create_table() >> extract_apod
+    api_response = extract_apod.output
+    transform_data = transform_apod_data(api_response)
+    load_data_to_postgres(transform_data)
      
